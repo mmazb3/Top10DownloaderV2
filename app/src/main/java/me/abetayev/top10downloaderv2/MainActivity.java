f6a65108f4e28088a3d5dc51654e98c4570c65c4
mmazb3 <murat@abetayev.me>
2018-02-26 17:27:36 +0600

    Initial commit

---
+++ app/src/main/java/me/abetayev/top10downloaderv2/MainActivity.java
@@ -0,0 +1,28 @@
+package me.abetayev.top10downloaderv2;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+    }
+}
+
+
